770cc962bbc22b3d2966d159ba623a4b44f210b6
Neema <x8c41@students.keele.ac.uk>
2025-11-13 18:06:39 +0000

    Comment on first file detailing Game requirements

---
+++ game.py
@@ -0,0 +1,7 @@
+# I plan on making GO using LLM's to demonstrate that I can
+# use git effectively. This will be my first commit and 
+# will contain only comments outlining my plan.
+
+# The game will need a backend for the game logic, a GUI for the board, 
+# a very simple bot to play against and checks to ensure the moves offered are 
+# valid.
